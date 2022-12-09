f2dde449128ce7a18ac50f4b3037ee2537f67dfb
Santiago Arechaga <email@nospam.com>
2022-12-09 16:58:53 +0100

    Eliminado servidor de prueba de Mastermind

--- Mastermind/Server/Mastermind server v0/Models/MoveResult.cs
+++
@@ -1,8 +0,0 @@
-namespace Daw2022.MastermindServer.Models;
-
-public class MoveResult
-{
-    public bool IsWinner { get ; set; }
-
-    public string[] Status { get ; set; } = Array.Empty<string>();
-} // class MoveResult
--- Mastermind/Server/Mastermind server v0/Models/PlayerMove.cs
+++
@@ -1,8 +0,0 @@
-namespace Daw2022.MastermindServer.Models;
-
-public class PlayerMove
-{
-    public Guid GameId { get ; set; }
-
-    public int[] Numbers { get ; set; } = new int[0];
-} // class PlayerMove
